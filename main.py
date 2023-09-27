3851122d698e6d5817cc24912bbeffc497e56a63
Filip Wysoczanski <filipwysoczanski308@gmail.com>
2023-09-27 15:57:37 +0200

    Cv2 Colorscale

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import cv2
 
 
-cap = cv2.VideoCapture(0, )
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+tracker = cv2.legacy.TrackerMOSSE()
+
+
+
 
 
 while True:
     ret, img = cap.read()
 
-    cv2.imshow('frame', img )
+    grayimage = cv2.cvtColor(img, cv2.COLOR_BGR2GRA)
+
+
+
+    cv2.imshow('frame', grayimage)
+
+
+
+
+
+
+    if cv2.waitKey(1) & ord('q'):
+        pass
 
 
